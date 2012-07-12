68b493fa762b5f1b8595951ed77d3363319f9df8
Mateusz Sojka <mateusz.sojka@infopark.de>
2012-07-12 20:45:00 +0200

    [Refactoring] New "tagged" way of searching for data

--- lib/yarg/data_store.rb
+++ lib/yarg/data_store.rb
@@ -6,51 +6,51 @@ module YARG
       def where(criteria = {})
         type = criteria[:type]
 
-        @files = []
-        get_files_for_key(type, mapping)
-        bb = @files.map { |file| fetch_data(file) }.flatten
+
+        files = get_files(criteria)
+
+        files.map { |file| fetch_data(file) }.flatten
       end
 
       private
 
-      def files
-        @files ||= []
+      # This method requires a criteria hash with a key-value relation.
+      # It iterates over tagged_data_files and gets their value hashes,
+      # which contain the matching tags. In the next step, it searches for
+      # the next not matching key and as soon as it finds one, it abort the
+      # bucle and set not_matching to the value of the first non matching tag
+      def get_files(criteria)
+        files = []
+
+        tagged_data_files.each do |file_name, tags|
+          not_matching = criteria.find do |key, value|
+            tags[key] != value
+          end
+
+          files << file_name if not_matching.nil?
+        end
+
+        files
       end
 
-      def get_files_for_key(key, hash)
-        hash[key] && hash[key].each do |key, value|
-          if value.is_a?(Hash)
-            get_files_for_key(key, { key => value })
-          elsif value.is_a?(Symbol)
-            files << value
+      def fetch_data(file_name)
+        [].tap do |data|
+          File.open("data/#{file_name.to_s}.txt").each_line do |line|
+            data << line.chomp
           end
         end
       end
 
-      def fetch_data(file_name)
-        data = []
-        File.open("data/#{file_name.to_s}.txt").each_line { |l| data << l.chomp }
-
-        data
-      end
-
-      def mapping
+      # This is a collection of data files which are used as data source
+      # for some of the random methods.
+      # A file needs to be stored under the /data directory in order to
+      # be recognized by the data store.
+      # The values are a hash of "tags" which define the data characteristics.
+      def tagged_data_files
         {
-          given_name: {
-            region: {
-              us: {
-                gender: {
-                  male: :given_names_male_us,
-                  female: :given_names_female_us
-                },
-              },
-            },
-          },
-          surname: {
-            region: {
-              us: :surnames_us
-            }
-          }
+          given_names_male_us: { type: :given_name, gender: :male, region: :us },
+          given_names_female_us: { type: :given_name, gender: :female, region: :us },
+          surnames_us: { type: :surname, region: :us }
         }
       end
 
--- spec/yarg/data_store_spec.rb
+++ spec/yarg/data_store_spec.rb
@@ -4,38 +4,39 @@ describe YARG::DataStore do
 
   describe 'DataStore.where(type: :given_name)' do
 
-    let :mapping do
+    let :tagged_data_files do
       {
-        given_name: {
-          region: {
-            us: {
-              gender: {
-                male: :given_names_male_us,
-                female: :given_names_female_us
-              },
-            },
-          },
-        },
-        surname: {
-          region: {
-            us: :surnames_us
-          }
-        }
+        given_names_male_us: { type: :given_name, gender: :male, region: :us },
+        given_names_female_us: { type: :given_name, gender: :female, region: :us },
+        surnames_us: { type: :surname, region: :us }
       }
+    end
+
+    before do
+      YARG::DataStore.stub(:tagged_data_files).and_return tagged_data_files
     end
 
     it 'returns an array of given names' do
       expected_names = [['Aaron', 'Mike'], ['Sarah']]
-      YARG::DataStore.stub(:mapping).and_return mapping
       YARG::DataStore.stub(:fetch_data).and_return *expected_names
 
       names = YARG::DataStore.where(type: :given_name)
       names.should == expected_names.flatten
     end
 
+    it 'returns an array of female given names' do
+      female_names = ['Adela', 'Nancey']
+      male_names = ['Thomas', 'John']
+
+      YARG::DataStore.stub(:fetch_data).with(:given_names_male_us).and_return male_names
+      YARG::DataStore.stub(:fetch_data).with(:given_names_female_us).and_return female_names
+
+      names = YARG::DataStore.where(type: :given_name, gender: :female)
+      names.should == female_names
+    end
+
     it 'returns an array of surnames' do
       expected_surnames = ['Smith', 'Locke']
-      YARG::DataStore.stub(:mapping).and_return mapping
       YARG::DataStore.stub(:fetch_data).and_return expected_surnames
 
       surnames = YARG::DataStore.where(type: :surname)
