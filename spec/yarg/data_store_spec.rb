ddbfb561b1deebeba271161f06c5cd8bf0844c66
Mateusz Sojka <mateusz.sojka@infopark.de>
2012-07-12 22:54:01 +0200

    [Refactoring] Use named subject in specs

--- spec/yarg/data_store_spec.rb
+++ spec/yarg/data_store_spec.rb
@@ -14,16 +14,18 @@ describe YARG::DataStore do
       }
     end
 
+    subject(:data_store) { YARG::DataStore }
+
     before do
-      YARG::DataStore.stub(:tagged_data_files).and_return tagged_data_files
-      YARG::DataStore.stub(:fetch_data).and_return []
+      data_store.stub(:tagged_data_files).and_return tagged_data_files
+      data_store.stub(:fetch_data).and_return []
     end
 
     it 'returns an array of given names' do
       expected_names = [['Aaron', 'Mike'], ['Sarah']]
-      YARG::DataStore.stub(:fetch_data).and_return *expected_names
+      data_store.stub(:fetch_data).and_return *expected_names
 
-      names = YARG::DataStore.where(type: :given_name)
+      names = data_store.where(type: :given_name)
       names.should == expected_names.flatten
     end
 
@@ -31,26 +33,26 @@ describe YARG::DataStore do
       female_names = ['Adela', 'Nancey']
       male_names = ['Thomas', 'John']
 
-      YARG::DataStore.stub(:fetch_data).with(:given_names_male_us).and_return male_names
-      YARG::DataStore.stub(:fetch_data).with(:given_names_female_us).and_return female_names
+      data_store.stub(:fetch_data).with(:given_names_male_us).and_return male_names
+      data_store.stub(:fetch_data).with(:given_names_female_us).and_return female_names
 
-      names = YARG::DataStore.where(type: :given_name, gender: :female)
+      names = data_store.where(type: :given_name, gender: :female)
       names.should == female_names
     end
 
     it 'returns an array of surnames' do
       expected_surnames = ['Smith', 'Locke']
-      YARG::DataStore.stub(:fetch_data).and_return expected_surnames
+      data_store.stub(:fetch_data).and_return expected_surnames
 
-      surnames = YARG::DataStore.where(type: :surname)
+      surnames = data_store.where(type: :surname)
       surnames.should == expected_surnames
     end
 
     it 'return an array of german given names' do
       expected_names = ['Hans', 'Sven']
-      YARG::DataStore.stub(:fetch_data).and_return expected_names
+      data_store.stub(:fetch_data).and_return expected_names
 
-      names = YARG::DataStore.where(type: :given_name, region: :de)
+      names = data_store.where(type: :given_name, region: :de)
       names.should == expected_names
     end
   end
