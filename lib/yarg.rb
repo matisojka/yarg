56a2bb20782f07c08e29bc43fbfd165e0ec7351e
Mateusz Sojka <yagooar@gmail.com>
2012-07-16 20:09:59 +0200

    DataStore should be loaded first

--- lib/yarg.rb
+++ lib/yarg.rb
@@ -1,6 +1,7 @@
+require_relative 'yarg/data_store'
+
 require_relative 'yarg/name'
 require_relative 'yarg/phone'
-require_relative 'yarg/data_store'
 require_relative 'yarg/country'
 
 require 'pry'
